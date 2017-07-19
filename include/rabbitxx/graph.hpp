ed9d36a0645e8e3cb78c5112ad1a65a3d9acebc2
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2017-07-19 14:27:44 +0200

    add helper functions to print enum values

--- include/rabbitxx/graph.hpp
+++ include/rabbitxx/graph.hpp
@@ -80,7 +80,7 @@ namespace rabbitxx {
     };
 
     // helper function, return string representation for io_operation_mode
-    std::string to_string(const otf2::common::io_operation_mode_type& mode)
+    std::string to_string(const otf2::common::io_operation_mode_type& mode) noexcept
     {
         switch (mode)
         {
@@ -95,7 +95,7 @@ namespace rabbitxx {
         }
     }
 
-    std::string to_string(const otf2::common::io_operation_flag_type& flag)
+    std::string to_string(const otf2::common::io_operation_flag_type& flag) noexcept
     {
         switch (flag)
         {
@@ -111,7 +111,7 @@ namespace rabbitxx {
     }
 
     // helper function, return string representation for io_seek_option_type
-    std::string to_string(const otf2::common::io_seek_option_type& op_type)
+    std::string to_string(const otf2::common::io_seek_option_type& op_type) noexcept
     {
         switch (op_type)
         {
@@ -130,7 +130,7 @@ namespace rabbitxx {
         }
     }
 
-    std::string to_string(const otf2::common::io_access_mode_type& acc_mode)
+    std::string to_string(const otf2::common::io_access_mode_type& acc_mode) noexcept
     {
         switch (acc_mode)
         {
@@ -149,7 +149,7 @@ namespace rabbitxx {
         }
     }
 
-    std::string to_string(const otf2::common::io_creation_flag_type& creation_flag)
+    std::string to_string(const otf2::common::io_creation_flag_type& creation_flag) noexcept
     {
         switch (creation_flag)
         {
@@ -182,7 +182,7 @@ namespace rabbitxx {
         }
     }
 
-    std::string to_string(const otf2::common::io_status_flag_type& status_flag)
+    std::string to_string(const otf2::common::io_status_flag_type& status_flag) noexcept
     {
         switch (status_flag)
         {
@@ -211,6 +211,150 @@ namespace rabbitxx {
         }
     }
 
+    std::string to_string(const otf2::common::collective_type& coll_type) noexcept
+    {
+        switch (coll_type)
+        {
+            case otf2::common::collective_type::barrier:
+                return {"barrier"};
+            case otf2::common::collective_type::broadcast:
+                return {"broadcast"};
+            case otf2::common::collective_type::gather:
+                return {"gather"};
+            case otf2::common::collective_type::gatherv:
+                return {"gatherv"};
+            case otf2::common::collective_type::scatter:
+                return {"scatter"};
+            case otf2::common::collective_type::scatterv:
+                return {"scatterv"};
+            case otf2::common::collective_type::all_gather:
+                return {"all gather"};
+            case otf2::common::collective_type::all_gatherv:
+                return {"all gatherv"};
+            case otf2::common::collective_type::all_to_all:
+                return {"all to all"};
+            case otf2::common::collective_type::all_to_allv:
+                return {"all to allv"};
+            case otf2::common::collective_type::all_to_allw:
+                return {"all to allw"};
+            case otf2::common::collective_type::all_reduce:
+                return {"all reduce"};
+            case otf2::common::collective_type::reduce_scatter:
+                return {"reduce scatter"};
+            case otf2::common::collective_type::scan:
+                return {"scan"};
+            case otf2::common::collective_type::exscan:
+                return {"exscan"};
+            case otf2::common::collective_type::reduce_scatter_block:
+                return {"reduce scatter block"};
+            case otf2::common::collective_type::create_handle:
+                return {"create handle"};
+            case otf2::common::collective_type::destroy_handle:
+                return {"destroy handle"};
+            case otf2::common::collective_type::allocate:
+                return {"allocate"};
+            case otf2::common::collective_type::deallocate:
+                return {"deallocate"};
+            case otf2::common::collective_type::create_handle_and_allocate:
+                return {"create handle and allocate"};
+            case otf2::common::collective_type::destroy_handle_and_deallocate:
+                return {"destroy handle and deallocate"};
+            default:
+                return {"UNDEFINED"};
+        }
+    }
+
+    std::string to_string(const otf2::common::group_type& grp_type) noexcept
+    {
+        switch (grp_type)
+        {
+            case otf2::common::group_type::unknown:
+                return {"unknown"};
+            case otf2::common::group_type::locations:
+                return {"locations"};
+            case otf2::common::group_type::regions:
+                return {"regions"};
+            case otf2::common::group_type::metric:
+                return {"metric"};
+            case otf2::common::group_type::comm_locations:
+                return {"comm locations"};
+            case otf2::common::group_type::comm_group:
+                return {"comm group"};
+            case otf2::common::group_type::comm_self:
+                return {"comm self"};
+            default:
+                return {"UNDEFINED"};
+        }
+    }
+
+    std::string to_string(const otf2::common::group_flag_type& grp_flag_type) noexcept
+    {
+        switch (grp_flag_type)
+        {
+            case otf2::common::group_flag_type::none:
+                return {"none"};
+            case otf2::common::group_flag_type::global_members:
+                return {"global members"};
+            default:
+                return {"UNDEFINED"};
+        }
+    }
+
+    std::string to_string(const otf2::common::paradigm_type& para_type) noexcept
+    {
+        switch (para_type)
+        {
+            case otf2::common::paradigm_type::unknown:
+                return {"unknown"};
+            case otf2::common::paradigm_type::user:
+                return {"user"};
+            case otf2::common::paradigm_type::compiler:
+                return {"compiler"};
+            case otf2::common::paradigm_type::openmp:
+                return {"openmp"};
+            case otf2::common::paradigm_type::mpi:
+                return {"mpi"};
+            case otf2::common::paradigm_type::cuda:
+                return {"cuda"};
+            case otf2::common::paradigm_type::measurement_system:
+                return {"measurement system"};
+            case otf2::common::paradigm_type::pthread:
+                return {"pthread"};
+            case otf2::common::paradigm_type::hmpp:
+                return {"hmpp"};
+            case otf2::common::paradigm_type::ompss:
+                return {"ompss"};
+            case otf2::common::paradigm_type::hardware:
+                return {"hardware"};
+            case otf2::common::paradigm_type::gaspi:
+                return {"gaspi"};
+            case otf2::common::paradigm_type::upc:
+                return {"upc"};
+            case otf2::common::paradigm_type::shmem:
+                return {"shmem"};
+            case otf2::common::paradigm_type::winthread:
+                return {"winthread"};
+            case otf2::common::paradigm_type::qtthread:
+                return {"qtthread"};
+            case otf2::common::paradigm_type::acethread:
+                return {"acethread"};
+            case otf2::common::paradigm_type::tbbthread:
+                return {"tbbthread"};
+            case otf2::common::paradigm_type::openacc:
+                return {"openacc"};
+            case otf2::common::paradigm_type::opencl:
+                return {"opencl"};
+            case otf2::common::paradigm_type::mtapi:
+                return {"mtapi"};
+            case otf2::common::paradigm_type::sampling:
+                return {"sampling"};
+            case otf2::common::paradigm_type::none:
+                return {"none"};
+            default:
+                return {"UNDEFINED"};
+        }
+    }
+
     struct io_creation_option_container
     {
         otf2::common::io_status_flag_type status_flag;
@@ -218,7 +362,7 @@ namespace rabbitxx {
         boost::optional<otf2::common::io_access_mode_type> access_mode;
 
         io_creation_option_container(const otf2::common::io_status_flag_type& status,
-                                     const otf2::common::io_creation_flag_type& flag, 
+                                     const otf2::common::io_creation_flag_type& flag,
                                      const otf2::common::io_access_mode_type& mode) noexcept
             : status_flag(status), creation_flag(flag), access_mode(mode)
         {
