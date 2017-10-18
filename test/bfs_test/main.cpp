c38cfd0a7e375df8853c15692c4ab633d4dc782d
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2017-10-18 13:05:24 +0200

    [Tests] implement simple bfs test

--- test/bfs_test/main.cpp
+++ test/bfs_test/main.cpp
@@ -1,15 +1,14 @@
-#include <boost/graph/depth_first_search.hpp>
-
 #include <rabbitxx/graph.hpp>
 #include <rabbitxx/log.hpp>
 
+#include <boost/graph/breadth_first_search.hpp>
 #include <boost/mpi.hpp>
 
 #include <iostream>
 
 using rabbitxx::logging;
 
-class dfs_print_visitor : public boost::default_dfs_visitor
+class bfs_print_visitor : public boost::default_bfs_visitor
 {
 public:
     template<typename Vertex, typename Graph>
@@ -18,16 +17,15 @@ public:
         if (g[v].type == rabbitxx::vertex_kind::sync_event)
         {
             auto vertex = boost::get<rabbitxx::sync_event_property>(g[v].property);
-            std::cout << "Descriptor: " << v << " process: " << vertex.proc_id << " Name: " << vertex.region_name << "\n";
+            std::cout << "Descriptor: " << v << " process: " << vertex.proc_id << " name: " << vertex.region_name << "\n";
         }
         else if (g[v].type == rabbitxx::vertex_kind::io_event)
         {
             auto vertex = boost::get<rabbitxx::io_event_property>(g[v].property);
-            std::cout << "Descriptor: " << v << " process: " << vertex.proc_id << " Name: " << vertex.region_name << "\n";
+            std::cout << "Descriptor: " << v << " process: " << vertex.proc_id << " name: " << vertex.region_name << "\n";
         }
     }
 };
-
 
 int main(int argc, char** argv)
 {
@@ -35,14 +33,14 @@ int main(int argc, char** argv)
     boost::mpi::communicator world;
 
     if (argc < 2) {
-        logging::fatal() << "usage: ./" << argv[0] << " <input-trace> ";
+        logging::fatal() << "usage ./" << argv[0] << " <input-trace> ";
         env.abort(1);
         return 1;
     }
 
     auto g = rabbitxx::make_graph<rabbitxx::graph::OTF2_Io_Graph_Builder>(argv[1], world);
-    dfs_print_visitor vis;
-    boost::depth_first_search(*g->get(), boost::visitor(vis));
+    bfs_print_visitor vis;
+    boost::breadth_first_search(*g->get(), vertex(0, *g->get()), boost::visitor(vis));
 
     return 0;
 }
