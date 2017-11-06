a0805c652e4a17d531fed51686f1f6f242202162
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2017-11-06 21:38:09 +0100

    [WIP] \o/ first concurrent io set implementation

--- include/rabbitxx/cio_set.hpp
+++ include/rabbitxx/cio_set.hpp
@@ -1,8 +1,14 @@
 #ifndef RABBITXX_CIO_SET_HPP
 #define RABBITXX_CIO_SET_HPP
 
+#include <rabbitxx/log.hpp>
+
+#include <boost/graph/depth_first_search.hpp>
+
 #include <map>
 #include <unordered_set>
+#include <vector>
+#include <algorithm>
 
 namespace rabbitxx {
 
@@ -50,12 +56,18 @@ class CIO_Set
         for (std::uint64_t i = 0; i < num_procs; ++i) {
             state_map_.insert(std::make_pair(i, Set_State::Open));
         }
-        start_evt_(start_event);
+        start_evt_ = start_event;
     }
 
     State_Map<Set_State> state_map() const noexcept
     {
         return state_map_;
+    }
+
+    Set_State state_for(std::uint64_t proc_id) noexcept
+    {
+        assert(state_map_.size() > proc_id);
+        return state_map_[proc_id];
     }
 
     EvtType start_event() const noexcept
@@ -82,6 +94,11 @@ class CIO_Set
     size_type size() const noexcept
     {
         return set_.size();
+    }
+
+    bool empty() const noexcept
+    {
+        return set_.empty();
     }
 
     auto insert(const value_type& value)
@@ -125,6 +142,231 @@ class CIO_Set
     EvtType end_evt_;
     set_t set_;
 };
+
+template<typename Set>
+std::string dump_state_map(const Set& set)
+{
+    std::stringstream sstr;
+    for (const auto& kvp : set.state_map()) {
+        sstr << kvp.first << ": [" << kvp.second << "] ";
+    }
+    return sstr.str();
+}
+
+template<typename EvtType, typename ValueType>
+inline std::ostream& operator<<(std::ostream& os, const CIO_Set<EvtType, ValueType>& set)
+{
+    os << "CIO_Set {\n"
+        << "[State] " << dump_state_map(set) << "\n"
+        << "[Start Evt] " << set.start_event() << "\n"
+        << "[End Evt] " << set.end_event() << "\n"
+        << "[Events] [ ";
+    std::copy(set.begin(), set.end(), 
+            std::ostream_iterator<ValueType>(os, ", "));
+    os << " ]\n};\n";
+    return os;
+}
+
+
+/**
+ * Utility function
+ * Finds the synthetic root node of a given graph.
+ * Root node is the only vertex with a `vertex_kind::synthetic`.
+ * Returns the vertex_descriptor of the root node.
+ */
+template<typename Graph>
+typename Graph::vertex_descriptor
+find_root(Graph& graph)
+{
+    const auto vertices = graph.vertices();
+    const auto root = std::find_if(vertices.first, vertices.second,
+            [&graph](const typename Graph::vertex_descriptor& vd) {
+                return graph[vd].type == rabbitxx::vertex_kind::synthetic;
+            });
+    return *root;
+}
+
+/**
+ * Get the number of processes involved, in a given I/O Graph.
+ * Take the synthetic root vertex and count the out_edges.
+ */
+template<typename Graph>
+std::uint64_t num_procs(Graph& graph) noexcept
+{
+    const auto root = find_root(graph);
+    return static_cast<std::uint64_t>(boost::out_degree(root, *graph.get()));
+}
+
+// TODO: remove rabbitxx:: namespace prefix while we are within this namespace!
+
+/**
+ * Return vector containing all the process id's of the processes participating
+ * within a given synchronization event.
+ */
+std::vector<std::uint64_t>
+procs_in_sync_involved(const rabbitxx::sync_event_property& sevt)
+{
+    std::uint64_t procs_involved {0};
+    if (sevt.comm_kind == rabbitxx::sync_event_kind::collective) {
+        const auto coll_evt = boost::get<rabbitxx::collective>(sevt.op_data);
+        return coll_evt.members();
+    }
+    else if (sevt.comm_kind == rabbitxx::sync_event_kind::p2p) {
+        const auto p2p_evt = boost::get<rabbitxx::peer2peer>(sevt.op_data);
+        return {sevt.proc_id, p2p_evt.remote_process()};
+    }
+}
+
+/**
+ * Return the number of processes involved in a given synchronization routine.
+ */
+std::uint64_t num_procs_in_sync_involved(const rabbitxx::sync_event_property& sevt)
+{
+    return procs_in_sync_involved(sevt).size();
+}
+
+template<typename Graph, typename Vertex, typename Visitor>
+void traverse_adjacent_vertices(Graph& graph, Vertex v, Visitor& vis)
+{
+    typename Graph::adjacency_iterator adj_begin, adj_end;
+    for (std::tie(adj_begin, adj_end) = boost::adjacent_vertices(v, *graph.get());
+         adj_begin != adj_end;
+        ++adj_begin)
+    {
+       //vis(graph, *adj_begin);
+       traverse_adjacent_vertices(graph, *adj_begin, vis);
+    }
+}
+
+//template<typename Outiter, typename Cont>
+template<typename Cont>
+class CIO_Visitor : public boost::default_dfs_visitor
+{
+    public:
+        //CIO_Visitor() : set_cnt_ptr_(std::make_shared<Cont>())
+        //{
+        //}
+
+        CIO_Visitor(std::shared_ptr<Cont>& sp, std::uint64_t np) : set_cnt_ptr_(sp), num_procs_(np)
+        {
+        }
+
+        template<typename Vertex, typename Graph>
+        void discover_vertex(Vertex v, const Graph& g)
+        {
+            auto *set_ptr = find_open_set_for(g[v].id());
+            if (set_ptr == nullptr) { // no open set was found for this process
+                logging::debug() << "No open set was found!";
+                if (vertex_kind::io_event == g[v].type) {
+                    logging::debug() << "I/O Event ... THIS SHOULD NEVER HAPPEN";
+                    return;
+                }
+                else if (vertex_kind::synthetic == g[v].type) {
+                    logging::debug() << "Synthetic Event ... create a new set";
+                    // create a new set
+                    set_cnt_ptr_->emplace_back(num_procs_, g[v]);
+                    //*set_ptr = set_cnt_ptr_->back(); CAUSES SEGV
+                }
+                else if (vertex_kind::sync_event == g[v].type) {
+                    // we are on a sync event and have no open set
+                    logging::debug() << "Sync Event ... create a new set";
+                    // create a new set and get a pointer to this set.
+                    set_cnt_ptr_->emplace_back(num_procs_, g[v]);
+                    //*set_ptr = set_cnt_ptr_->back(); CAUSES SEGV
+                }
+            }
+            else { // an open set for this process was found
+                logging::debug() << "Open set found!";
+                if (vertex_kind::io_event == g[v].type) {
+                    logging::debug() << "I/O Event ... ";
+                    // open set and on I/O event -> insert I/O event into set
+                    set_ptr->insert(v);
+                    logging::debug() << "insert vertex #" << v << " into current set";
+                }
+                else if (vertex_kind::synthetic == g[v].type) {
+                    logging::debug() << "Synthetic Event ... THIS SHOULD NEVER HAPPEN";
+                    return;
+                }
+                else if (vertex_kind::sync_event == g[v].type) {
+                    logging::debug() << "Sync Event ... close current set @" << g[v].id();
+                    // open set and on sync event -> close set
+                    set_ptr->close(g[v].id());
+                    // if have end event, check if current sync event is successors of the end event!
+                    set_ptr->set_end_event(g[v]);
+                    logging::debug() << *set_ptr;
+                    logging::debug() << "create a new set!";
+                    //TODO do not create every time a new set
+                    set_cnt_ptr_->emplace_back(num_procs_, g[v]);
+
+                }
+            }
+        }
+
+        template<typename Edge, typename Graph>
+        void examine_edge(Edge e, const Graph& g)
+        {
+        }
+
+        template<typename Edge, typename Graph>
+        void tree_edge(Edge e, const Graph& g)
+        {
+        }
+
+    private:
+        //template<typename Set>
+        auto find_open_set_for(const std::uint64_t proc_id) -> typename Cont::value_type*
+        {
+            auto it = std::find_if(std::begin(*set_cnt_ptr_.get()), std::end(*set_cnt_ptr_.get()),
+                    [&proc_id](typename Cont::value_type& set) {
+                        return Set_State::Open == set.state_for(proc_id);
+                    });
+            if (it == std::end(*set_cnt_ptr_.get())) {
+                return nullptr;
+            }
+            return &(*it);
+        }
+
+    private:
+        std::shared_ptr<Cont> set_cnt_ptr_;
+        std::uint64_t num_procs_;
+        //Outiter out_;
+};
+
+//template<typename Out>
+//auto make_cio_visitor(Out out)
+//{
+    //CIO_Visitor<Outiter> vis(out);
+    //return vis;
+//}
+
+template<typename Graph>
+auto collect_concurrent_io_sets(Graph& graph)
+{
+    //std::vector<CIO_Set<
+        //otf2_trace_event,
+        //typename Graph::vertex_descriptor>> set_v;
+
+    using set_container_t = std::vector<CIO_Set<
+                                otf2_trace_event,
+                                typename Graph::vertex_descriptor>>;
+
+    auto root = find_root(graph);
+    const auto np = num_procs(graph);
+
+    auto shared_set_container(std::make_shared<set_container_t>());
+    CIO_Visitor<set_container_t> vis(shared_set_container, np);
+
+    logging::debug() << "first vertex at: " << graph[root].id() << " [" << graph[root].name() << "]";
+    logging::debug() << "START";
+
+    //traverse_adjacent_vertices(graph, root, vis);
+
+    std::vector<boost::default_color_type> color_map(graph.num_vertices());
+    boost::depth_first_visit(*graph.get(), root, vis,
+            make_iterator_property_map(color_map.begin(), get(boost::vertex_index, *graph.get())));
+
+    return shared_set_container;
+}
 
 } // namespace rabbitxx
 
--- modules/concurrent_io_sets/main.cpp
+++ modules/concurrent_io_sets/main.cpp
@@ -1,143 +1,10 @@
 #include <rabbitxx/graph.hpp>
+#include <rabbitxx/cio_set.hpp>
 #include <rabbitxx/log.hpp>
+
 #include <boost/mpi.hpp>
 
-#include <iostream>
-#include <algorithm>
-#include <string>
-
 using rabbitxx::logging;
-
-/**
- * First try to define a container for concurrent I/O operation sets.
- * Here we use a std::unordered_set as a basis. This might be good for inserting,
- * but there might be problems when searching or some ordering is needed.
- */
-#include <unordered_set>
-#include <vector>
-#include <memory>
-
-/**
- * Utility function
- * Finds the synthetic root node of a given graph.
- * Root node is the only vertex with a `vertex_kind::synthetic`.
- * Returns the vertex_descriptor of the root node.
- */
-template<typename Graph>
-typename Graph::vertex_descriptor
-find_root(Graph& graph)
-{
-    const auto vertices = graph.vertices();
-    const auto root = std::find_if(vertices.first, vertices.second,
-            [&graph](const typename Graph::vertex_descriptor& vd) {
-                return graph[vd].type == rabbitxx::vertex_kind::synthetic;
-            });
-    return *root;
-}
-
-/**
- * Return vector containing all the process id's of the processes participating
- * within a given synchronization event.
- */
-std::vector<std::uint64_t>
-procs_in_sync_involved(const rabbitxx::sync_event_property& sevt)
-{
-    std::uint64_t procs_involved {0};
-    if (sevt.comm_kind == rabbitxx::sync_event_kind::collective) {
-        const auto coll_evt = boost::get<rabbitxx::collective>(sevt.op_data);
-        return coll_evt.members();
-    }
-    else if (sevt.comm_kind == rabbitxx::sync_event_kind::p2p) {
-        const auto p2p_evt = boost::get<rabbitxx::peer2peer>(sevt.op_data);
-        return {sevt.proc_id, p2p_evt.remote_process()};
-    }
-}
-
-/**
- * Return the number of processes involved in a given synchronization routine.
- */
-std::uint64_t num_procs_in_sync_involved(const rabbitxx::sync_event_property& sevt)
-{
-    return procs_in_sync_involved(sevt).size();
-}
-
-template<typename Graph, typename Vertex, typename Visitor>
-void traverse_adjacent_vertices(Graph& graph, Vertex v, Visitor& vis)
-{
-    typename Graph::adjacency_iterator adj_begin, adj_end;
-    for (std::tie(adj_begin, adj_end) = boost::adjacent_vertices(v, *graph.get());
-         adj_begin != adj_end;
-        ++adj_begin)
-    {
-       vis(graph, *adj_begin);
-       traverse_adjacent_vertices(graph, *adj_begin, vis);
-    }
-}
-
-/**
- * Own Concurrent-I/O-Set Visitor
- * TODO: look at boost::default_{bfs,dfs}_visitor and the generic boost graph
- * visitor concept!
- */
-template<typename Graph>
-class CIO_Visitor
-{
-    using cio_set = std::unordered_set<typename Graph::vertex_descriptor>;
-    using cio_container = std::vector<cio_set>;
-public:
-
-    CIO_Visitor() : current_(std::make_unique<cio_set>()), cio_cnt_()
-    {
-    }
-
-    template<typename Vertex>
-    void operator()(const Graph& graph, const Vertex v)
-    {
-        if (graph[v].type == rabbitxx::vertex_kind::io_event)
-        {
-            // add to set, if no current set create a new one
-            add_to_set(v);
-            logging::debug() << "added vertex: #" << v << " @ "
-                << graph[v].id() << " [" << graph[v].name() << "]" << " to current set";
-        }
-        else if (graph[v].type == rabbitxx::vertex_kind::sync_event)
-        {
-            const auto sync_event = boost::get<rabbitxx::sync_event_property>(graph[v].property);
-            // distinguish between p2p and collective sync event
-            logging::debug() << "discovered sync event vertex: #" << v << " @ " << graph[v].id()
-                << " [" << graph[v].name() << "] CLOSE SET #" << num_procs_in_sync_involved(sync_event);
-        }
-    }
-private:
-    template<typename Vertex>
-    void add_to_set(Vertex v)
-    {
-        assert(current_.get() != nullptr);
-        current_->insert(v);
-    }
-
-    void close_set()
-    {
-        assert(current_.get() != nullptr);
-        cio_cnt_.push_back(*current_.get());
-        current_.reset(new cio_set());
-    }
-
-private:
-    std::unique_ptr<cio_set> current_;
-    cio_container cio_cnt_;
-};
-
-
-template<typename Graph>
-void collect_concurrent_sets(Graph& graph)
-{
-    CIO_Visitor<Graph> vis;
-    auto root = find_root(graph);
-    logging::debug() << "first vertex at: " << graph[root].id() << " [" << graph[root].name() << "]";
-    logging::debug() << "START";
-    traverse_adjacent_vertices(graph, root, vis);
-}
 
 int main(int argc, char** argv)
 {
@@ -152,7 +19,31 @@ int main(int argc, char** argv)
     }
 
     auto graph = rabbitxx::make_graph<rabbitxx::graph::OTF2_Io_Graph_Builder>(argv[1], world);
-    collect_concurrent_sets(*graph.get());
+    //rabbitxx::collect_concurrent_sets(*graph.get());
+    //rabbitxx::type_printer<typename decltype(graph)::element_type::vertex_descriptor> tp;
+
+    //std::vector<rabbitxx::CIO_Set<
+        //rabbitxx::otf2_trace_event,
+        //typename decltype(graph)::element_type::vertex_descriptor>> setv;
+
+    auto io_sets = rabbitxx::collect_concurrent_io_sets(*graph.get());
+    logging::debug() << "\n\n";
+    //FIXME remove empty sets
+    io_sets->erase(std::remove_if(
+                io_sets->begin(),
+                io_sets->end(),
+                [](const auto& set) { return set.empty(); }),
+                io_sets->end());
+
+
+    for (const auto& set : *io_sets) {
+        std::cout << set;
+        std::cout << "Events:\n";
+        for (auto evt : set) {
+            std::cout << "@" << graph->operator[](evt).id()
+                << " Name: " << graph->operator[](evt).name() << "\n";
+        }
+    }
 
     return 0;
 }
