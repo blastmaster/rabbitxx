ae5dd7682569e0f1625be940a90ced45ff50faf0
kali pso <kalipso@c3d2.de>
2017-12-07 10:11:12 +0000

    init test_data

---
+++ test/test_data/2.cc
@@ -0,0 +1,48 @@
+#include <mpi.h>
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[])
+{
+    MPI_Init(NULL, NULL);
+
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    if(world_size < 4){
+        std::cout << "This Programm is designed for 4 processes" << std::endl;
+        MPI_Abort(MPI_COMM_WORLD, 1);
+        return 0;
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    fflush(stdout);
+
+    for (int i = 0; i < 2; ++i) {
+        int number = 1;
+
+        switch(world_rank){
+            case 0: MPI_Recv(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    break;
+            case 1: MPI_Send(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+                    break;
+            case 2: MPI_Recv(&number, 1, MPI_INT, 3, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    break;
+            case 3: MPI_Send(&number, 1, MPI_INT, 2, 0, MPI_COMM_WORLD);
+                    break;
+            default: std::cout << "World Rank not found, use -n 4" << std::endl;
+                    break;
+        }
+
+        fflush(stdout);
+    }
+
+
+    if(world_rank != 2){ fflush(stdout); }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Finalize();
+
+    return 0;
+}
---
+++ test/test_data/README.md
@@ -0,0 +1,8 @@
+dios-trace-simple_barrier_new.png = 1.xml (carefull: absolute paths are wrong)
+trace-edgecase.png = 2.cc
+trace-edgecase_hard.png = 3.cc
+trace-own_trace6.png = 4.cc
+trace-own_trace_advanced_6.png = 5.cc
+
+trace-advanced-20*.png is not possible anymore, it depends on an older version of dios.
+detailed description of the tests will come soon
