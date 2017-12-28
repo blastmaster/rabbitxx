c2230664a5b48e588f4e83c8258a191dc732a53e
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2017-12-28 21:28:35 +0100

    [WIP] remove redundant `virtual`
    * Use the `virtual` keyword is redundant if already using `override`.

--- include/rabbitxx/graph/builder/otf2_io_graph_builder.hpp
+++ include/rabbitxx/graph/builder/otf2_io_graph_builder.hpp
@@ -134,7 +134,7 @@ namespace rabbitxx { namespace graph {
 
     public:
         // Events
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                            const otf2::event::enter& evt) override
         {
             logging::trace() << "Found enter event to location #" << location.ref() << " @"
@@ -146,7 +146,7 @@ namespace rabbitxx { namespace graph {
             region_name_queue_.enqueue(location, evt.region().name().str());
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                            const otf2::event::leave& evt) override
         {
             logging::trace() << "Found leave event to location #" << location.ref() << " @"
@@ -158,7 +158,7 @@ namespace rabbitxx { namespace graph {
             region_name_queue_.dequeue(location);
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                            const otf2::event::io_operation_begin& evt) override
         {
             logging::trace() << "Found io_operation_begin event to location #" << location.ref() << " @"
@@ -172,7 +172,7 @@ namespace rabbitxx { namespace graph {
             io_ops_started_.enqueue(location, evt);
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                            const otf2::event::io_operation_complete& evt) override
         {
             logging::trace() << "Found io_operation_complete event to location #" << location.ref() << " @"
@@ -198,7 +198,7 @@ namespace rabbitxx { namespace graph {
             io_ops_started_.dequeue(location);
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::io_acquire_lock& evt) override
         {
             logging::trace() << "Found io_acquire_lock event to location #" << location.ref() << " @"
@@ -209,7 +209,7 @@ namespace rabbitxx { namespace graph {
             //graph_.add_vertex();
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::io_change_status_flag& evt) override
         {
             logging::trace() << "Found io_change_status_flag event to location #" << location.ref() << " @"
@@ -220,7 +220,7 @@ namespace rabbitxx { namespace graph {
             //graph_.add_vertex();
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::io_create_handle& evt) override
         {
             logging::trace() << "Found io_create_handle event to location #" << location.ref() << " @"
@@ -251,7 +251,7 @@ namespace rabbitxx { namespace graph {
             events_.enqueue(location, descriptor);
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::io_delete_file& evt) override
         {
             logging::trace() << "Found io_delete_file event to location #" << location.ref() << " @"
@@ -278,7 +278,7 @@ namespace rabbitxx { namespace graph {
             events_.enqueue(location, descriptor);
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::io_destroy_handle& evt) override
         {
             logging::trace() << "Found io_destroy_handle event to location #" << location.ref() << " @"
@@ -306,7 +306,7 @@ namespace rabbitxx { namespace graph {
             events_.enqueue(location, descriptor);
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::io_duplicate_handle& evt) override
         {
             logging::trace() << "Found io_duplicate_handle event to location #" << location.ref() << " @"
@@ -325,7 +325,7 @@ namespace rabbitxx { namespace graph {
             events_.enqueue(location, descriptor);
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::io_operation_cancelled& evt) override
         {
             logging::trace() << "Found io_operation_cancelled event to location #" << location.ref() << " @"
@@ -336,7 +336,7 @@ namespace rabbitxx { namespace graph {
             //graph_.add_vertex();
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::io_operation_issued& evt) override
         {
             logging::trace() << "Found io_operation_issued event to location #" << location.ref() << " @"
@@ -347,7 +347,7 @@ namespace rabbitxx { namespace graph {
             //graph_.add_vertex();
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::io_operation_test& evt) override
         {
             logging::trace() << "Found io_operation_test event to location #" << location.ref() << " @"
@@ -358,7 +358,7 @@ namespace rabbitxx { namespace graph {
             //graph_.add_vertex();
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::io_release_lock& evt) override
         {
             logging::trace() << "Found io_release_lock event to location #" << location.ref() << " @"
@@ -369,7 +369,7 @@ namespace rabbitxx { namespace graph {
             //graph_.add_vertex();
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::io_seek& evt) override
         {
             logging::trace() << "Found io_seek event to location #" << location.ref() << " @"
@@ -391,7 +391,7 @@ namespace rabbitxx { namespace graph {
             events_.enqueue(location, descriptor);
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::io_try_lock& evt) override
         {
             logging::trace() << "Found io_try_lock event to location #" << location.ref() << " @"
@@ -402,7 +402,7 @@ namespace rabbitxx { namespace graph {
             //graph_.add_vertex();
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::mpi_collective_begin& evt) override
         {
             logging::trace() << "Found mpi_collective_begin event to location #" << location.ref() << " @"
@@ -413,7 +413,7 @@ namespace rabbitxx { namespace graph {
             mpi_coll_started_.enqueue(location, evt);
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::mpi_collective_end& evt) override
         {
             logging::trace() << "Found mpi_collective_end event to location #" << location.ref() << " @"
@@ -459,7 +459,7 @@ namespace rabbitxx { namespace graph {
             events_.enqueue(location, descriptor);
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::mpi_ireceive& evt) override
         {
             logging::trace() << "Found mpi_ireceive event to location #" << location.ref() << " @"
@@ -478,7 +478,7 @@ namespace rabbitxx { namespace graph {
             events_.enqueue(location, descriptor);
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::mpi_ireceive_request& evt) override
         {
             logging::trace() << "Found mpi_ireceive_request event to location #" << location.ref() << " @"
@@ -488,7 +488,7 @@ namespace rabbitxx { namespace graph {
             //graph_.add_vertex();
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::mpi_isend& evt) override
         {
             logging::trace() << "Found mpi_isend event to location #" << location.ref() << " @"
@@ -507,7 +507,7 @@ namespace rabbitxx { namespace graph {
             events_.enqueue(location, descriptor);
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::mpi_isend_complete& evt) override
         {
             logging::trace() << "Found mpi_isend_complete event to location #" << location.ref() << " @"
@@ -517,7 +517,7 @@ namespace rabbitxx { namespace graph {
             //graph_.add_vertex();
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::mpi_receive& evt) override
         {
             logging::trace() << "Found mpi_receive event to location #" << location.ref() << " @"
@@ -535,7 +535,7 @@ namespace rabbitxx { namespace graph {
             events_.enqueue(location, descriptor);
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::mpi_request_cancelled& evt) override
         {
             logging::trace() << "Found mpi_request_cancelled event to location #" << location.ref() << " @"
@@ -545,7 +545,7 @@ namespace rabbitxx { namespace graph {
             //graph_.add_vertex();
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::mpi_request_test& evt) override
         {
             logging::trace() << "Found mpi_request_test event to location #" << location.ref() << " @"
@@ -555,7 +555,7 @@ namespace rabbitxx { namespace graph {
             //graph_.add_vertex();
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::mpi_send& evt) override
         {
             logging::trace() << "Found mpi_send event to location #" << location.ref() << " @"
@@ -573,7 +573,7 @@ namespace rabbitxx { namespace graph {
             events_.enqueue(location, descriptor);
         }
 
-        virtual void event(const otf2::definition::location& location,
+        void event(const otf2::definition::location& location,
                             const otf2::event::unknown& evt) override
         {
             logging::warn() << "Found unknown event with timestamp " << evt.timestamp()
@@ -583,7 +583,7 @@ namespace rabbitxx { namespace graph {
             //graph_.add_vertex();
         }
 
-        virtual void events_done(const otf2::reader::reader& rdr) override
+        void events_done(const otf2::reader::reader& rdr) override
         {
             auto k_map = get(&otf2_trace_event::type, *(graph_->get())); //get property map of vertex kinds {SYNC,IO, SYNTHETIC}
             if (is_master())
@@ -689,48 +689,48 @@ namespace rabbitxx { namespace graph {
 
         // Definitions
 
-        virtual void definition(const otf2::definition::string& definition) override
+        void definition(const otf2::definition::string& definition) override
         {
             logging::trace() << "Found string defintion";
         }
 
-        virtual void definition(const otf2::definition::location& definition) override
+        void definition(const otf2::definition::location& definition) override
         {
             logging::trace() << "Found location defintion";
             locations_.push_back(definition);
         }
 
-        virtual void definition(const otf2::definition::region& definition) override
+        void definition(const otf2::definition::region& definition) override
         {
             logging::trace() << "Found region defintion";
         }
 
-        virtual void definition(const otf2::definition::comm& definition) override
+        void definition(const otf2::definition::comm& definition) override
         {
             logging::trace() << "Found comm defintion";
         }
 
-        virtual void definition(const otf2::definition::io_paradigm& definition) override
+        void definition(const otf2::definition::io_paradigm& definition) override
         {
             logging::trace() << "Found io_paradigm defintion";
         }
 
-        virtual void definition(const otf2::definition::io_handle& definition) override
+        void definition(const otf2::definition::io_handle& definition) override
         {
             logging::trace() << "Found io_handle defintion";
         }
 
-        virtual void definition(const otf2::definition::io_file_property& definition) override
+        void definition(const otf2::definition::io_file_property& definition) override
         {
             logging::trace() << "Found io_file_property defintion";
         }
 
-        virtual void definition(const otf2::definition::unknown& definition) override
+        void definition(const otf2::definition::unknown& definition) override
         {
             logging::warn() << "Found unknown defintion";
         }
 
-        virtual void definitions_done(const otf2::reader::reader& rdr) override
+        void definitions_done(const otf2::reader::reader& rdr) override
         {
             for(auto location : rdr.locations()) {
                 //do rank mapping!
