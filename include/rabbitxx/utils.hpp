800c19dac5a04971d82728be008b55431e287b00
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2018-03-19 10:12:17 +0100

    add duration to string helper

--- include/rabbitxx/utils.hpp
+++ include/rabbitxx/utils.hpp
@@ -7,6 +7,14 @@ namespace rabbitxx {
 
 namespace fs = boost::filesystem;
 
+template<typename DurationT=otf2::chrono::nanoseconds>
+std::string duration_to_string(const otf2::chrono::duration& dur)
+{
+    std::stringstream ss;
+    ss << otf2::chrono::duration_cast<DurationT>(dur);
+    return ss.str();
+}
+
 }
 
 #endif // RABBITXX_UTILS_HPP
