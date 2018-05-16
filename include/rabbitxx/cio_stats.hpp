28aee7f567f15777c7698990203c1c2a82fdcf73
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2018-05-16 19:26:44 +0200

    [HACK] fix cio set duration
    * When a CIO-Set starts with a synthetic event kind the timestamps are
    wrong. So if we encounter such a set take the first or last event
    timestamp in the set respectively.

--- include/rabbitxx/cio_stats.hpp
+++ include/rabbitxx/cio_stats.hpp
@@ -223,12 +223,26 @@ bool file_has_shared_access(const IoGraph& graph,
 otf2::chrono::microseconds
 get_set_duration(const IoGraph& graph, const set_t<VertexDescriptor>& cio_set)
 {
-    auto ts_start = graph[cio_set.start_event()].timestamp();
-    auto ts_end = graph[cio_set.end_event().value()].timestamp();
+    auto start_vd = cio_set.start_event();
+    auto end_vd = cio_set.end_event().value();
+    auto ts_start = graph[start_vd].timestamp();
+    auto ts_end = graph[end_vd].timestamp();
+    if (graph[start_vd].type == vertex_kind::synthetic)
+    {
+        //take timestamp of first event in set
+        //std::cerr << "set start event ts from: " << ts_start;
+        ts_start = graph[*cio_set.begin()].timestamp();
+        //std::cerr << " to " << ts_start << "\n";
+    }
+    if (graph[end_vd].type == vertex_kind::synthetic)
+    {
+        //std::cerr << "set end event ts from: " << ts_end;
+        ts_end = graph[*std::prev(cio_set.end())].timestamp();
+        //std::cerr << " to " << ts_end << "\n";
+    }
     assert(ts_end > ts_start);
     auto dur_diff = ts_end - ts_start;
-    auto res = otf2::chrono::duration_cast<otf2::chrono::microseconds>(dur_diff);
-    return res;
+    return otf2::chrono::duration_cast<otf2::chrono::microseconds>(dur_diff);
 }
 
 class CIO_Stats
