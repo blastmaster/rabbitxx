55198d17f88d2eb44de1939d56fa53f681a0868a
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2018-06-10 17:33:14 +0200

    Enhance set2csv
    * Using Experiment-API
    * csv output for summary - this seems to be a death-end and might be
      removed in the future.

--- modules/set2csv/main.cpp
+++ modules/set2csv/main.cpp
@@ -1,4 +1,4 @@
-#include <rabbitxx/cio_stats.hpp>
+#include <rabbitxx/experiment.hpp>
 
 /**
  * Module to serialize sets to csv
@@ -88,12 +88,99 @@ void set2csv(const IoGraph& graph, const set_t<VertexDescriptor>& set, std::ostr
 
 }
 
-static std::string create_filename(std::uint64_t sidx)
+static std::string create_cio_set_stats_filename(std::uint64_t sidx)
 {
     std::stringstream sstr;
     sstr << "set-" << sidx << ".csv";
 
     return sstr.str();
+}
+
+void sets_2_csv(const IoGraph& graph, const set_container_t<VertexDescriptor>& cio_sets,
+         const fs::path& base_path)
+{
+    //header();
+    std::uint64_t idx = 1;
+    for (const auto& set : cio_sets)
+    {
+        std::string filename = create_cio_set_stats_filename(idx);
+        std::ofstream ofile((base_path / filename).string());
+        set2csv(graph, set, ofile);
+        ++idx;
+    }
+}
+
+void pio_sets_2_csv(const IoGraph& graph, const set_map_t<VertexDescriptor>& pio_sets,
+        const fs::path& base_path)
+{
+    for (const auto p_kvp : pio_sets)
+    {
+        fs::path proc_dir = base_path / std::to_string(p_kvp.first);
+        fs::create_directory(proc_dir);
+        sets_2_csv(graph, p_kvp.second, proc_dir);
+    }
+}
+
+void cio_stats_2_csv(const CIO_Stats& stats, std::ostream& out)
+{
+    out << "Number of CIO-Sets," << stats.number_of_cio_sets() << "\n"
+        << "Build time," << stats.build_time() << "\n"
+        << "Set Durations,";
+    const auto set_durs = stats.get_set_durations();
+    // one comma to much
+    std::copy(set_durs.begin(), set_durs.end(),
+            std::ostream_iterator<otf2::chrono::duration>(out, ","));
+    out << "\n";
+}
+
+void pio_stats_2_csv(const PIO_Stats& stats, std::ostream& out)
+{
+    out << "Number of PIO-Sets," << stats.number_of_pio_sets() << "\n"
+        << "PIO-Sets build time," << stats.build_time() << "\n";
+}
+
+void graph_stats_2_csv(const Graph_Stats& stats, std::ostream& out)
+{
+    out << "Number of Vertices," << stats.number_of_vertices() << "\n"
+        << "Number of Edges," << stats.number_of_edges() << "\n"
+        << "Graph Build Time," << stats.build_time() << "\n"
+        << "Ticks per Second," << stats.ticks_per_second().count() << "\n"
+        << "Start Time," << stats.start_time().count() << "\n"
+        << "Length," << stats.length().count() << "\n";
+}
+
+void experiment_stats_2_csv(const Experiment_Stats& stats, std::ostream& out)
+{
+    out << "Tracefile," << stats.trace_file() << "\n"
+        << "Experiment Duration," << stats.experiment_duration() << "\n";
+    graph_stats_2_csv(stats.graph_stats(), out);
+    cio_stats_2_csv(stats.cio_stats(), out);
+    pio_stats_2_csv(stats.pio_stats(), out);
+}
+
+void summary_2_csv(const Experiment_Stats& stats, const fs::path& base_path)
+{
+    std::ofstream out_file((base_path / "summary.csv").string());
+    experiment_stats_2_csv(stats, out_file);
+}
+
+void csv_output(const Experiment::experiment_results& res,
+        const Experiment_Stats& stats,
+        const experiment_config& config,
+        const fs::path& base_path)
+{
+    if (config.pio_sets)
+    {
+        pio_sets_2_csv(res.graph, res.pio_sets, base_path / "pio-sets");
+    }
+    if (config.cio_sets)
+    {
+        sets_2_csv(res.graph, res.cio_sets, base_path /  "cio-sets");
+    }
+    if (config.with_summary)
+    {
+        summary_2_csv(stats, base_path);
+    }
 }
 
 int main(int argc, char** argv)
@@ -104,19 +191,8 @@ int main(int argc, char** argv)
         return EXIT_FAILURE;
     }
 
-    std::string trc_file(argv[1]);
-    auto graph = make_graph<graph::OTF2_Io_Graph_Builder>(trc_file);
-    auto cio_sets = find_cio_sets(graph);
-
-    //header();
-    std::uint64_t idx = 1;
-    for (const auto& set : cio_sets)
-    {
-        std::string filename = create_filename(idx);
-        std::ofstream ofile(filename);
-        set2csv(graph, set, ofile);
-        ++idx;
-    }
-
+    fs::path trc_file(argv[1]);
+    Experiment exp(trc_file); // just trace file use default config
+    auto stats = exp.run(csv_output);
     return EXIT_SUCCESS;
 }
