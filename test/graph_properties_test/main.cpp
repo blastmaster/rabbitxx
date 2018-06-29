6c228ef7463e48310e080f62077dddfa32631f63
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2018-06-29 19:10:29 +0200

    [Test] use ostream<< operator

--- test/graph_properties_test/main.cpp
+++ test/graph_properties_test/main.cpp
@@ -3,30 +3,9 @@
 
 using rabbitxx::logging;
 
-
-template<typename DurationT=otf2::chrono::microseconds>
-void print_info_as(const rabbitxx::app_info& info)
-{
-    std::cout << "total time: " << std::chrono::duration_cast<DurationT>(info.total_time) << "\n";
-    std::cout << "total file io time: " << std::chrono::duration_cast<DurationT>(info.io_time) << "\n";
-    std::cout << "total file io metadata time: " << std::chrono::duration_cast<DurationT>(info.io_metadata_time) << "\n";
-    std::cout << "first event time: " << info.first_event_time << "\n";
-    std::cout << "last event time: " << info.last_event_time << "\n";
-
-    std::cout << "first event time duration: " 
-        << std::chrono::duration_cast<DurationT>(info.first_event_time.time_since_epoch()) << "\n";
-    std::cout << "last event time duration: " << std::chrono::duration_cast<DurationT>(info.last_event_time.time_since_epoch()) << "\n";
-
-    std::cout << "Clock properties:\n"
-        << "ticks per second: " << info.clock_props.ticks_per_second().count() << "\n"
-        << "start time: " << info.clock_props.start_time().count() << "\n"
-        << "length: " << info.clock_props.length().count() << "\n";
-}
-
 void print_graph_properties(const rabbitxx::IoGraph& graph)
 {
-    auto info = graph.graph_properties();
-    print_info_as(info);
+    std::cout << graph.graph_properties();
 }
 
 int main(int argc, char** argv)
