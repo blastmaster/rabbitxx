e7733ef109088021f13644ed42cc986a15f1fb20
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2019-09-23 11:23:11 +0200

    [analysis] use default for experiment

--- scripts/analysis/experiment.py
+++ scripts/analysis/experiment.py
@@ -10,11 +10,7 @@ class ExperimentStats:
         self.tracefile = kw['Tracefile']
         self.clock_properties = kw['Clock Properties']
         self.file_map = kw['File Map']
-        try:
-            self.num_locations = kw['Number of Locations']
-        except KeyError:
-            print('Number of locations is not included in summary file.')
-            self.num_locations = "NaN"
+        self.num_locations = kw.get('Number of Locations', 'NaN')
         self.graph_stats = graph_stats
         self.cio_stats = cio_stats
         self.pio_stats = pio_stats
